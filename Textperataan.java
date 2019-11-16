383f08a68045949cd4f22fec6851a24b1e68d865
JessicaFransisca51017012 <48231202+JessicaFransisca51017012@users.noreply.github.com>
2019-11-16 15:21:48 +0800

    Add files via upload

---
+++ Textperataan.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textperataan;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.FontMetrics;
+import java.awt.Frame;
+import java.awt.Panel;
+
+public class Textperataan extends Panel {
+    Textperataan(){
+    setBackground(Color.GRAY); //Konstanta dalam class Color
+}
+    public void paint(Graphics g){
+        Font f = new Font ("Helvetica", Font.BOLD, 48);
+        FontMetrics fm = getFontMetrics(f);
+        g.setFont(f);
+        
+        String S = "teks ini tampil di tengah";
+        int xpos = (this.size().width - fm.stringWidth(S)) / 2;
+        int ypos = (this.size().height) / 2;
+        g.drawString(S, xpos, ypos);
+    }
+    public static void main(String[] args) {
+        Frame f = new Frame("Testing Graphics Panel");
+        Textperataan gp = new Textperataan();
+        f.add(gp);
+        f.setSize(600, 300);
+        f.setVisible(true);
+    }
+    
+}
